33f5bfe80c23fd0dc91786b9a2f7b7ee49ab8122
Hanifemam <h_emamgholizadeh.com>
2025-10-25 13:15:58 +0000

    First generative model debugged with smaller model

--- intro/first_text_generation.py
+++ intro/first_text_generation.py
@@ -1,1 +1,11 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import pipeline
+
+model = AutoModelForCausalLM.from_pretrained("TinyLlama/TinyLlama-1.1B-Chat-v1.0", device_map="cuda", torch_dtype="auto", trust_remote_code=True)
+tokenizer = AutoTokenizer.from_pretrained("TinyLlama/TinyLlama-1.1B-Chat-v1.0")
+
+generator = pipeline("text-generation", model=model, tokenizer=tokenizer, return_full_text=False, max_new_tokens=500, do_sample=False)
+
+messages = [{"role": "user", "content": "Create a funny jock about chikens."}]
+output = generator(messages)
+print(output[0]['generated_text'])
